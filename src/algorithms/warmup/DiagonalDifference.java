029333b78335082c8ad2fee662f6bb190fdde2e7
dannypadilla <disavowed10@gmail.com>
2016-07-20 14:19:45 -0700

    Initial commit

---
+++ src/algorithms/warmup/DiagonalDifference.java
@@ -0,0 +1,27 @@
+package algorithms.warmup;
+
+/*
+ * ========== Diagonal Difference ==========
+ * 
+ * Given a square matrix of size N x N, calculate the absolute difference between the sums of its diagonals.
+ * 
+ * Input:
+ * The first line contains a single integer, N. The next N lines denote the matrix's rows,
+ * with each line containing N space-separated integers describing the columns.
+ * 
+ * Output:
+ * Print the absolute difference between the two sums of the matrix's diagonals as a single integer.
+ * 
+ */
+
+import java.util.Scanner;
+
+public class DiagonalDifference {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);						// Prompt for user input
+		
+	}
+
+}
